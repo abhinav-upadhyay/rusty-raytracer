f521c0575ab30b87d55e418442250ebe53028ce7
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2021-01-07 21:37:28 +0530

    Update the intersection API as per the test spec.
    Each intersection object should contain only one intersection point and
    the intersect method should return a vector of such intersections.

--- src/intersection.rs
+++ src/intersection.rs
@@ -1,26 +1,26 @@
 use super::ray::Ray;
 
 pub trait Intersect<T> {
-    fn intersect(&self, ray: &Ray) -> Intersection<T>;
+    fn intersect(&self, ray: &Ray) -> Vec<Intersection<T>>;
 }
 
 pub struct Intersection<'a, T> {
     object: &'a T,
-    points: Vec<f32>
+    point: f32
 }
 
 impl<'a, T> Intersection<'a, T> {
 
-    pub fn new(object: &'a T, points: Vec<f32>) -> Self {
-        Self {object, points}
+    pub fn new(object: &'a T, point: f32) -> Self {
+        Self {object, point}
     }
 
     pub fn object(&self) -> &T {
         self.object
     }
 
-    pub fn points(&self) -> &Vec<f32> {
-        &self.points
+    pub fn point(&self) -> f32 {
+        self.point
     }
 }
 
@@ -32,9 +32,9 @@ mod tests {
     #[test]
     fn test_intersection_constructor() {
         let s = Sphere::new(1);
-        let i: Intersection<Sphere> = Intersection::new(&s, vec![3.1]);
+        let i: Intersection<Sphere> = Intersection::new(&s, 3.1);
         assert_eq!(i.object(), &Sphere::new(1));
-        assert_eq!(i.points(), &vec![3.1]);
+        assert_eq!(i.point(), 3.1);
     }
 
 }
--- src/sphere.rs
+++ src/sphere.rs
@@ -15,8 +15,8 @@ impl Sphere {
 
 }
 
-impl Intersect<Sphere> for Sphere {
-    fn intersect(&self, ray: &Ray) -> Intersection<Self> {
+impl Intersect<Self> for Sphere {
+    fn intersect(&self, ray: &Ray) -> Vec<Intersection<Self>> {
         let sphere_to_ray = ray.origin() - &Tuple::point(0.0, 0.0, 0.0);
         let a = ray.direction().dot(ray.direction());
         let b = 2.0 * ray.direction().dot(&sphere_to_ray);
@@ -24,19 +24,19 @@ impl Intersect<Sphere> for Sphere {
         let discriminant = b * b - 4.0 * a * c;
 
         if discriminant < 0.0 {
-            return Intersection::new(self, Vec::new());
+            return Vec::new();
         }
         let mut xs = Vec::with_capacity(2);
         let x1 = (-b - discriminant.sqrt()) / (2.0 * a);
         let x2 = (-b + discriminant.sqrt()) / (2.0 * a);
         if x1 < x2 {
-            xs.push(x1);
-            xs.push(x2);
+            xs.push(Intersection::new(self, x1));
+            xs.push(Intersection::new(self, x2));
         } else {
-            xs.push(x2);
-            xs.push(x1);
+            xs.push(Intersection::new(self, x2));
+            xs.push(Intersection::new(self, x1));
         }
-        return Intersection::new(self, xs);
+        return xs;
 
     }
 }
@@ -49,28 +49,28 @@ mod tests {
     fn test_ray_intersection() {
         let s = Sphere::new(1);
         let r = Ray::new(Tuple::point(0.0, 0.0, -5.0), Tuple::vector(0.0, 0.0, 1.0));
-        let intersection = s.intersect(&r);
-        assert_eq!(intersection.points().len(), 2);
-        assert_eq!(intersection.points()[0], 4.0);
-        assert_eq!(intersection.points()[1], 6.0);
+        let intersections = s.intersect(&r);
+        assert_eq!(intersections.len(), 2);
+        assert_eq!(intersections[0].point(), 4.0);
+        assert_eq!(intersections[1].point(), 6.0);
     }
 
     #[test]
     fn test_tangent_intersection() {
         let s = Sphere::new(1);
         let r = Ray::new(Tuple::point(0.0, 1.0, -5.0), Tuple::vector(0.0, 0.0, 1.0));
-        let intersection = s.intersect(&r);
-        assert_eq!(intersection.points().len(), 2);
-        assert_eq!(intersection.points()[0], 5.0);
-        assert_eq!(intersection.points()[1], 5.0);
+        let intersections = s.intersect(&r);
+        assert_eq!(intersections.len(), 2);
+        assert_eq!(intersections[0].point(), 5.0);
+        assert_eq!(intersections[1].point(), 5.0);
     }
 
     #[test]
     fn test_no_intersection() {
         let s = Sphere::new(1);
         let r = Ray::new(Tuple::point(0.0, 2.0, -5.0), Tuple::vector(0.0, 0.0, 1.0));
-        let intersection = s.intersect(&r);
-        assert_eq!(intersection.points().len(), 0);
+        let intersections = s.intersect(&r);
+        assert_eq!(intersections.len(), 0);
     }
 
     #[test]
@@ -78,10 +78,10 @@ mod tests {
         //ray originates from within the sphere - we still get two intersections
         let s = Sphere::new(1);
         let r = Ray::new(Tuple::point(0.0, 0.0, 0.0), Tuple::vector(0.0, 0.0, 1.0));
-        let intersection = s.intersect(&r);
-        assert_eq!(intersection.points().len(), 2);
-        assert_eq!(intersection.points()[0], -1.0);
-        assert_eq!(intersection.points()[1], 1.0);
+        let intersections = s.intersect(&r);
+        assert_eq!(intersections.len(), 2);
+        assert_eq!(intersections[0].point(), -1.0);
+        assert_eq!(intersections[1].point(), 1.0);
     }
 
     #[test]
@@ -89,10 +89,10 @@ mod tests {
         // if the sphere lies behind the ray, even then we get two intersections with negative values
         let s = Sphere::new(1);
         let r = Ray::new(Tuple::point(0.0, 0.0, 5.0), Tuple::vector(0.0, 0.0, 1.0));
-        let intersection = s.intersect(&r);
-        assert_eq!(intersection.points().len(), 2);
-        assert_eq!(intersection.points()[0], -6.0);
-        assert_eq!(intersection.points()[1], -4.0);
+        let intersections = s.intersect(&r);
+        assert_eq!(intersections.len(), 2);
+        assert_eq!(intersections[0].point(), -6.0);
+        assert_eq!(intersections[1].point(), -4.0);
     }
 
 }
